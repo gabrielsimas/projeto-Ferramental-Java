064a30f43d23bc15247cfb6f4066fd47793c8370
gabrielsimas <gabrielsimas@77eb020a-e6db-cc4c-d23b-33725f1e001e>
2012-05-01 22:28:09 +0000

    Commit GerarRelatorio.
    Serializada.

--- Ferramental-DESENVOLVIMENTO/src/br/com/medralservicosrio/controlador/GerarRelatorio.java
+++ Ferramental-DESENVOLVIMENTO/src/br/com/medralservicosrio/controlador/GerarRelatorio.java
@@ -1,27 +1,10 @@
 package br.com.medralservicosrio.controlador;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import net.sf.jasperreports.engine.JRException;
-import br.com.medralservicosrio.conexao.ConnectionFactory;
-import br.com.medralservicosrio.util.ReportUtils;
 
 
 public class GerarRelatorio extends HttpServlet {
+
+	private static final long serialVersionUID = 8705961602216202465L;
 	
 }
