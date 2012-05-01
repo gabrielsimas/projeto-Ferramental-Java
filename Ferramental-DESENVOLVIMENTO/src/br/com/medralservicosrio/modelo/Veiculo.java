ee41b0df06be2bfc4fe3c6e5b01876bb1aaae659
gabrielsimas <gabrielsimas@77eb020a-e6db-cc4c-d23b-33725f1e001e>
2012-05-01 19:06:14 +0000

    Commit da Classe Veiculo.

--- Ferramental-DESENVOLVIMENTO/src/br/com/medralservicosrio/modelo/Veiculo.java
+++ Ferramental-DESENVOLVIMENTO/src/br/com/medralservicosrio/modelo/Veiculo.java
@@ -5,95 +5,35 @@
 package br.com.medralservicosrio.modelo;
 
 /**
- *
- * @author Eliane
+ * Classe de Persistência Estoque
+ * 
+ * @author 		Luis Gabriel Nascimento Simas
+ * @category 	Classe Persistente
+ * @since		12/04/2012	
+ * 
+ * Padrão de Classe JavaBean contendo:
+ * Serialização
+ * Construtor Vazio
+ * Construtor com parâmetro
+ * Getters
+ * Setters
+ * Override de Métodos toString, hashCode e equals
+ * 
+ *  Este padrão serve para qualquer projeto Java até EJB
  */
 
 public class Veiculo {
-    private int idVeiculo;
+    private Integer idVeiculo;
     private String placa;
     private String marca;
     private String modelo;
-    private String setor;
-    private int matrEncarregado;
+    private String setor; //TODO: Criar uma Tabela Primária para receber o código dos setores
+    private Integer matriculaFuncionario;
     private boolean status;
     
     public Veiculo(){
-        
+    	
     }
-
-    public Veiculo(int idVeiculo, String placa, String marca, String modelo, String setor, int matrEncarregado, boolean status) {
-        this.idVeiculo = idVeiculo;
-        this.placa = placa;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.setor = setor;
-        this.matrEncarregado = matrEncarregado;
-        this.status = status;
-    }
-
-    public int getIdVeiculo() {
-        return idVeiculo;
-    }
-
-    public void setIdVeiculo(int idVeiculo) {
-        this.idVeiculo = idVeiculo;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public int getMatrEncarregado() {
-        return matrEncarregado;
-    }
-
-    public void setMatrEncarregado(int matrEncarregado) {
-        this.matrEncarregado = matrEncarregado;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getPlaca() {
-        return placa;
-    }
-
-    public void setPlaca(String placa) {
-        this.placa = placa;
-    }
-
-    public String getSetor() {
-        return setor;
-    }
-
-    public void setSetor(String setor) {
-        this.setor = setor;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public boolean validar(Veiculo veiculo) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-    
-    
-    
     
     
 }
