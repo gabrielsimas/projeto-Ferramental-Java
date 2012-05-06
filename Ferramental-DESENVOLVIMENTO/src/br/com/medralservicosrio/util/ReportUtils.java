62102fa86f732753863db25c43619c40c2a19f91
felltsp@gmail.com <felltsp@gmail.com@77eb020a-e6db-cc4c-d23b-33725f1e001e>
2012-05-06 02:02:13 +0000

    felipe.tavares: atualizado modos de geração de relatórios.

--- Ferramental-DESENVOLVIMENTO/src/br/com/medralservicosrio/util/ReportUtils.java
+++ Ferramental-DESENVOLVIMENTO/src/br/com/medralservicosrio/util/ReportUtils.java
@@ -1,18 +1,24 @@
 package br.com.medralservicosrio.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Connection;
+import java.util.Collection;
 import java.util.Map;
 
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRExporter;
 import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 
 
@@ -53,4 +59,80 @@ public class ReportUtils {
     return out;
     }
 
+    /**
+	 * Gera o relatório em PDF e exporta para o response.
+	 * @param reportPath Caminho do Relatório
+	 * @param fileName Nome do arquivo que será exibido para download
+	 * @param data Dados a serem exibidos
+	 * @param params Parâmetros para o relatório
+	 * @param request Objeto Request
+	 * @param response Objeto response
+	 * @return Bytes do Arquivo PDF gerado
+	 * @throws JRException Erro ao gerar relatório
+	 * @throws IOException Erro ao escrever response
+	 */
+	public static void gerarPdf(String reportPath, String fileName, Collection data, Map params, HttpServletRequest request, HttpServletResponse response) throws JRException, IOException {
+		InputStream is = request.getSession().getServletContext().getResourceAsStream(reportPath);
+		
+		response.setContentType("application/pdf");
+		response.setHeader("Content-disposition","attachment; filename=\""+ fileName+ ".pdf\"");  
+
+		JasperPrint print = JasperFillManager.fillReport(is, params,  new JRBeanCollectionDataSource(data));
+		JasperExportManager.exportReportToPdfStream(print, response.getOutputStream());
+		
+		response.getOutputStream().flush();
+		response.getOutputStream().close();
+	}
+	
+	/**
+	 * Gera o relatório em PDF e exporta para o response.
+	 * @param reportPath Caminho do Relatório
+	 * @param fileName Nome do arquivo que será exibido para download
+	 * @param data Dados a serem exibidos
+	 * @param params Parâmetros para o relatório
+	 * @param context Instância do FacesContext
+	 * @return Bytes do Arquivo PDF gerado
+	 * @throws JRException Erro ao gerar relatório
+	 * @throws IOException Erro ao escrever response
+	 */
+	public static void gerarPdf(String reportPath, String fileName, Collection data, Map params, FacesContext context) throws JRException, IOException {
+		
+		HttpServletRequest request   = (HttpServletRequest) context.getExternalContext().getRequest();		
+		HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+		
+		InputStream is = request.getSession().getServletContext().getResourceAsStream(reportPath);
+		
+		response.setContentType("application/pdf");
+		response.setHeader("Content-disposition","attachment; filename=\""+ fileName+ ".pdf\"");  
+
+		JasperPrint print = JasperFillManager.fillReport(is, params,  new JRBeanCollectionDataSource(data));
+		JasperExportManager.exportReportToPdfStream(print, response.getOutputStream());
+		
+		response.getOutputStream().flush();
+		response.getOutputStream().close();
+		
+		context.renderResponse();
+		context.responseComplete(); 
+	}
+	
+	/**
+	 * Gera o relatório em PDF e retorna os bytes.
+	 * @param reportPath Caminho Relatório
+	 * @param data Dados a serem exibidos
+	 * @param params Parâmetros para o relatório
+	 * @param request Objeto Request
+	 * @return Bytes do Arquivo PDF gerado
+	 * @throws JRException Erro ao gerar relatório
+	 * @throws IOException Erro ao escrever response
+	 */
+	public static byte[] gerarPdf(String reportPath, Collection data, Map params, HttpServletRequest request) throws JRException, IOException {
+		InputStream is = request.getSession().getServletContext().getResourceAsStream(reportPath);
+		
+		JasperPrint print = JasperFillManager.fillReport(is, params,  new JRBeanCollectionDataSource(data));
+		
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		JasperExportManager.exportReportToPdfStream(print, os);
+		
+		return os.toByteArray();
+	}
 }
